7dda5c31c986b57f376386efe7d8409c2adb8a02
Carson O'Sullivan <66914659+yoshirozay@users.noreply.github.com>
2023-05-26 12:52:23 -0400

    Initial Commit

---
+++ RemotionTest/RemotionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemotionTestApp.swift
+//  RemotionTest
+//
+//  Created by Carson O'Sullivan on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RemotionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
